4160fa7eade3ba236995bd00a08398fd9a3bc1c4
nauderuan777 <nauderuan777@gmail.com>
2016-10-04 16:57:13 +0200

    v11
    v11

--- addon/components/bloep-input-text.js
+++ addon/components/bloep-input-text.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import layout from '../templates/components/bloep-input-text';
 import masterFart from 'mdb-input-master/components/bloep-inout-master';
 
-export default Ember.Component.extend(masterFart, {
+export default masterFart.extend( {
   layout
 
 });
